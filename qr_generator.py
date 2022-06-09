d33f207f27fa7821adf3517a3fbccd209bbb65b1
Haokun Cai <99503764+haokun-cai-capstone@users.noreply.github.com>
2022-06-09 12:58:18 -0700

    Add files via upload

---
+++ qr_generator.py
@@ -0,0 +1,25 @@
+# qr code generation script
+# takes a xlsx file and generate qr code for each row
+
+import qrcode
+import pandas as pd
+
+
+#replace this by the actual excel path
+read_file = pd.read_excel ("C:/Users/haokun/OneDrive - UW/Desktop/qr generation/aircraftnum.xlsx")
+for row in read_file['aircraft number']:
+    print(row)
+    qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+    )
+
+    labeldata = row
+
+    qr.add_data(labeldata)
+    qr.make(fit=True)
+
+    img = qr.make_image()
+    img.save("{}.jpg".format(labeldata))
